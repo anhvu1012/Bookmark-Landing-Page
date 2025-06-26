50e6e89dd1da61978e02c788fddf4840ccb5ce94
Anh <92798974+anhvu1012@users.noreply.github.com>
2025-06-26 16:02:46 +0200

    Add feature section + its feature choosing operation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Operation of opening & closing mobile navigation
 const mobileNavOpenBtn = document.getElementById('mobile-nav-open-btn');
 const mobileNavCloseBtn = document.getElementById('mobile-nav-close-btn');
 const mobileNav = document.getElementById('mobile-nav-wrapper');
@@ -8,4 +9,30 @@ mobileNavOpenBtn.addEventListener('click', (e) => {
 mobileNavCloseBtn.addEventListener('click', (e) => {
   e.preventDefault();
   mobileNav.classList.add('hidden');
+});
+
+// Operation of choosing feature
+const featureOptions = Array.from(document.querySelectorAll('.feature-option'));
+const featureOptionInfos = Array.from(document.querySelectorAll('.feature-option-info'));
+// console.log(featureOptionInfos)
+const removeOptionActive = () => {
+  featureOptions.forEach(option => {
+    option.classList.remove('option-active');
+  });
+};
+const showFeatureInfo = (optionId) => {
+  featureOptionInfos.forEach(info => {
+    info.classList.add('hidden');
+  });
+  featureOptionInfos[optionId].classList.remove('hidden');
+};
+
+featureOptions.forEach((option, id) => {
+  option.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    removeOptionActive();
+    option.classList.add('option-active');
+    showFeatureInfo(id);
+  });
 });
